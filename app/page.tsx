d01e7d53d3edc32e1f61973caa6c32d63cee6ed5
umarameen <umarameen026@gmail.com>
2025-01-22 13:35:45 +0530

    push youtubeApp

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { videos } from "./data/data";
+import SearchBar from "./searchBar";
+
+import Youtube from "./youtube";
+
+export default function Home() {
+  return (
+    <div>
+      <SearchBar/>
+      <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+        {videos.map((video) => (
+          <Youtube key={video.id} {...video} />
+        ))}
+      </div>
+    </div>
+  );
+}
