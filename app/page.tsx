07b37f437bcc52a4547074c3814ec492f1ea899b
umarameen <umarameen026@gmail.com>
2025-02-05 11:30:41 +0530

    commit searchBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { videos } from "./data/data";
 import SearchBar from "./searchBar";
-
 import Youtube from "./youtube";
 
 export default function Home() {
   return (
     <div>
-      <SearchBar/>
+       <SearchBar /> 
       <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
         {videos.map((video) => (
           <Youtube key={video.id} {...video} />
--- app/searchBar.tsx
+++ app/searchBar.tsx
@@ -1,3 +1,18 @@
-export default  function SearchBar() {
-  return <input placeholder="search" />;
+"use client";
+
+import { videos } from "./data/data";
+
+export default function SearchBar() {
+  
+//  const ListItems = videos.filter((list) => {
+//     list.id.indexOf() > 
+//   }); 
+
+  return (
+    <>
+      <form className="flex space-x-2">
+        <input placeholder="search..."  />
+      </form>
+    </>
+  );
 }
